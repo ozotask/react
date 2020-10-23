90bc48167977c2e1ea42775bbcc8368bbb5af15b
ozotask <ozotask@naver.com>
2020-10-23 12:48:40 +0900

    프로젝트업로드

---
+++ precache-manifest.67e8fafe48abcd094fd9f3ad4d36ff7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dce9f7d4a519eb273a11bc2216ca189",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d641f14826a5f279352",
+    "url": "/static/css/main.5e468b30.chunk.css"
+  },
+  {
+    "revision": "f1b97dc8d5ce8967b879",
+    "url": "/static/js/2.b3d3bc0a.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.b3d3bc0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d641f14826a5f279352",
+    "url": "/static/js/main.33270ca9.chunk.js"
+  },
+  {
+    "revision": "b95b5309348bfb8514a1",
+    "url": "/static/js/runtime-main.dcfef9df.js"
+  }
+]);
